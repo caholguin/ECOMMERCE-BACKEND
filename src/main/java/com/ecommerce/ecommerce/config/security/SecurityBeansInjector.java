4a26b979a2dca262bf2d7731810c40b84d694690
Carlos Holguin <caholguin1798@gmail.com>
2025-03-11 17:24:52 -0500

    Se crea Authentication Provider

--- src/main/java/com/ecommerce/ecommerce/config/security/SecurityBeansInjector.java
+++ src/main/java/com/ecommerce/ecommerce/config/security/SecurityBeansInjector.java
@@ -3,19 +3,24 @@ package com.ecommerce.ecommerce.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class SecurityBeansInjector {
 
-    private final AuthenticationConfiguration authenticationConfiguration;
-
-    public SecurityBeansInjector(AuthenticationConfiguration authenticationConfiguration){
-        this.authenticationConfiguration = authenticationConfiguration;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception{
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider();
+        authenticationStrategy.setPasswordEncoder(null);
+        authenticationStrategy.setUserDetailsService(null);
+
+        return authenticationStrategy;
     }
 }
