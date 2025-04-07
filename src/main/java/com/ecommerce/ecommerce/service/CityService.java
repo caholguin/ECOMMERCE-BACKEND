a9cbce550eda299337ab33fc34a0f05e7859b29a
Carlos Holguin <caholguin1798@gmail.com>
2025-04-07 16:35:15 -0500

    Avances en funcionalidad de envios

--- src/main/java/com/ecommerce/ecommerce/service/CityService.java
+++ src/main/java/com/ecommerce/ecommerce/service/CityService.java
@@ -1,8 +1,11 @@
 package com.ecommerce.ecommerce.service;
 
+import com.ecommerce.ecommerce.dto.response.CityDTO;
 import com.ecommerce.ecommerce.entity.City;
 
 public interface CityService {
+
+    CityDTO findById(Long id);
 
     City findByEntity(Long id);
 }
--- src/main/java/com/ecommerce/ecommerce/service/impl/CityServiceImpl.java
+++ src/main/java/com/ecommerce/ecommerce/service/impl/CityServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.ecommerce.service.impl;
 
+import com.ecommerce.ecommerce.dto.response.CityDTO;
 import com.ecommerce.ecommerce.entity.City;
 import com.ecommerce.ecommerce.exception.ObjectNotFoundException;
+import com.ecommerce.ecommerce.mapper.CityMapper;
 import com.ecommerce.ecommerce.repository.CityRepository;
 import com.ecommerce.ecommerce.service.CityService;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,11 @@ public class CityServiceImpl implements CityService {
 
     public CityServiceImpl(CityRepository cityRepository){
         this.cityRepository = cityRepository;
+    }
+
+    @Override
+    public CityDTO findById(Long id){
+        return CityMapper.toDto(this.findByEntity(id));
     }
 
     @Override
