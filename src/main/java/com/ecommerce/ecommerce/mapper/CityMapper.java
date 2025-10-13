c153c293aa16065e8c82316a2e3b8a69611aac24
Carlos Holguin <caholguin1798@gmail.com>
2025-10-13 10:58:41 -0500

    Se implementa CRUD ciudades

--- src/main/java/com/ecommerce/ecommerce/mapper/CityMapper.java
+++ src/main/java/com/ecommerce/ecommerce/mapper/CityMapper.java
@@ -1,8 +1,12 @@
 package com.ecommerce.ecommerce.mapper;
 
+import com.ecommerce.ecommerce.dto.request.SaveCategoryDTO;
+import com.ecommerce.ecommerce.dto.request.SaveCityDTO;
 import com.ecommerce.ecommerce.dto.response.AddressDTO;
 import com.ecommerce.ecommerce.dto.response.CityDTO;
+import com.ecommerce.ecommerce.entity.Category;
 import com.ecommerce.ecommerce.entity.City;
+import com.ecommerce.ecommerce.entity.Family;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +21,23 @@ public class CityMapper {
         cityDTO.setPrice(city.getPrice());
 
         return cityDTO;
+    }
+
+    public static City toEntity(SaveCityDTO saveCityDTO){
+        if (saveCityDTO == null) return null;
+
+        City city = new City();
+        city.setName(saveCityDTO.getName());
+        city.setPrice(saveCityDTO.getPrice());
+
+        return city;
+    }
+
+    public static void updateEntity(City city, SaveCityDTO saveCityDTO){
+        if(city == null || saveCityDTO == null) return;
+
+        city.setName(saveCityDTO.getName());
+        city.setPrice(saveCityDTO.getPrice());
     }
 
     public static AddressDTO.CityDTO toGetCityDto(City city){
